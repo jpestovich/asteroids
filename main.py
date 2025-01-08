51a9d0e85699ad24710e48557ce19f220c4a4988
jpestovich <jpestovich@protonmail.com>
2025-01-07 21:42:49 -0700

    background color

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from constants import *
+import pygame
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(True):
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.update()
+    #print("Starting asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()

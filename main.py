7a3de88dd3e16ae083f96124f243c0ee22591d01
jpestovich <jpestovich@protonmail.com>
2025-01-07 21:58:06 -0700

    add circleshape

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from constants import *
+from circleshape import *
 import pygame
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #set fps
+    clock = pygame.time.Clock()
+    dt = 0 
+    #while loop for game time
     while(True):
+        #prevent problems with exiting
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        #set background here
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.update()
+        dt = clock.tick(60) / 1000
     #print("Starting asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
